ff7f06ad7840cbdeef41ad1879e9746233ac006f
Shuwan Zhao <shuwanzhao@Mac.ht.home>
2025-11-19 23:04:17 -0500

    create event

---
+++ app/components/ui/select.tsx
@@ -0,0 +1,158 @@
+// app/components/ui/select.tsx
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface SelectContextValue {
+  value: string;
+  onValueChange: (value: string) => void;
+}
+
+const SelectContext = React.createContext<SelectContextValue | undefined>(undefined);
+
+const Select = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & {
+    value?: string;
+    onValueChange?: (value: string) => void;
+  }
+>(({ className, value = "", onValueChange, ...props }, ref) => {
+  const [internalValue, setInternalValue] = React.useState(value);
+
+  const handleValueChange = (newValue: string) => {
+    setInternalValue(newValue);
+    onValueChange?.(newValue);
+  };
+
+  return (
+    <SelectContext.Provider value={{ value: internalValue, onValueChange: handleValueChange }}>
+      <div ref={ref} className={cn("relative", className)} {...props} />
+    </SelectContext.Provider>
+  );
+});
+Select.displayName = "Select";
+
+const SelectTrigger = React.forwardRef<
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+>(({ className, children, ...props }, ref) => {
+  const context = React.useContext(SelectContext);
+  if (!context) throw new Error("SelectTrigger must be used within Select");
+
+  return (
+    <button
+      ref={ref}
+      className={cn(
+        "flex h-10 w-full items-center justify-between rounded-md border border-gray-300 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      {...props}
+    >
+      {children}
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-4 w-4 opacity-50"
+      >
+        <path
+          d="M4.5 6.5L7.5 9.5L10.5 6.5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </button>
+  );
+});
+SelectTrigger.displayName = "SelectTrigger";
+
+const SelectValue = React.forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement> & {
+    placeholder?: string;
+  }
+>(({ className, placeholder, ...props }, ref) => {
+  const context = React.useContext(SelectContext);
+  if (!context) throw new Error("SelectValue must be used within Select");
+
+  return (
+    <span
+      ref={ref}
+      className={cn("block truncate", className)}
+      {...props}
+    >
+      {context.value || placeholder}
+    </span>
+  );
+});
+SelectValue.displayName = "SelectValue";
+
+const SelectContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, children, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "absolute z-50 min-w-[8rem] overflow-hidden rounded-md border border-gray-200 bg-white text-gray-950 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 top-full mt-1 w-full",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </div>
+));
+SelectContent.displayName = "SelectContent";
+
+const SelectItem = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & {
+    value: string;
+  }
+>(({ className, value, children, ...props }, ref) => {
+  const context = React.useContext(SelectContext);
+  if (!context) throw new Error("SelectItem must be used within Select");
+
+  const isSelected = context.value === value;
+
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-gray-100 focus:text-gray-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        isSelected && "bg-red-50 text-red-600",
+        className
+      )}
+      onClick={() => context.onValueChange(value)}
+      {...props}
+    >
+      <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+        {isSelected && (
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4"
+          >
+            <path
+              d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        )}
+      </span>
+      {children}
+    </div>
+  );
+});
+SelectItem.displayName = "SelectItem";
+
+export { Select, SelectTrigger, SelectValue, SelectContent, SelectItem };
